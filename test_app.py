3312a1c09d54beeb88597b4b3a5d67e03f910492
KimGR25 <rudfhr25@gmail.com>
2023-04-19 16:25:56 +0900

    tessst2

--- test_app.py
+++ test_app.py
@@ -18,7 +18,7 @@ from app import hello
 class TestHelloApp(unittest.TestCase):
 
   def test_hello(self):
-    self.assertEqual(hello(), "Hello CI/CD!!!!\n")
+    self.assertEqual(hello(), "Hello Git Hub!!!!")
 
 if __name__ == '__main__':
   unittest.main()
